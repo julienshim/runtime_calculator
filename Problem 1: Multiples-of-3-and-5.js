db310cdb4d1285d5c9bd89539eab4bdb12a5abb0
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-01-16 09:49:15 -0800

    Update Problem 1: Multiples-of-3-and-5.js
    forgot to console.log/return

--- Problem 1: Multiples-of-3-and-5.js
+++ Problem 1: Multiples-of-3-and-5.js
@@ -1,8 +1,9 @@
-function sum (max) {
+function sum35(max) {
   let sum = 0;
   for (let i = 1; i < max; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     };
-  };
+  }
+ console.log(sum);
 }
