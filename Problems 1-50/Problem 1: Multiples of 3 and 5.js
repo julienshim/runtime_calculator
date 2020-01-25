22de164afd44e7b156a9edba10bdebe0ce1d5e9d
Julien Shim <25723428+julienshim@users.noreply.github.com>
2020-01-25 13:10:38 -0800

    Update Problem 1: Multiples of 3 and 5.js
    converted from eulerproj site to fcc

--- Problems 1-50/Problem 1: Multiples of 3 and 5.js
+++ Problems 1-50/Problem 1: Multiples of 3 and 5.js
@@ -1,9 +1,12 @@
-function sum35(max) {
+function multiplesOf3and5(number) {
+  // Good luck!
   let sum = 0;
-  for (let i = 1; i < max; i++) {
+  for (let i = 1; i < number; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
       sum += i;
     };
   }
- console.log(sum);
+ return sum
 }
+
+multiplesOf3and5(1000);
